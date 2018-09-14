439512f046dd5cbccdabfe8b314713bcb4b34728
kamalnrf <kamalnrf@hotmail.com>
2018-09-15 03:09:20 +0530

    Updates with set project name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const QUESTIONS = [
   {
     name: "which-directory",
     type: "input",
-    message: "Do you want new directory (Yes/No):",
+    message: "Do you want a new directory (Yes/No):",
     validate: function(input) {
       if (/^(?:Yes|No)$/.test(input) || /^(?:yes|no)$/.test(input)) return true;
       else return "Enter either Yes or No";
@@ -38,7 +38,14 @@ inquirer.prompt(QUESTIONS).then(answers => {
     fs.mkdirSync(projectPath);
   }
 
-  projectPath !== '' ? createDirectoryContents(templatePath, projectPath) : null
+  projectPath !== ""
+    ? createDirectoryContents(templatePath, projectPath)
+    : null;
+  fs.writeFileSync(
+    projectPath + "/package.json",
+    JSON.stringify(pkg(projectName), null, 2),
+    "utf8"
+  );
 });
 
 function createDirectoryContents(templatePath, newProjectPath) {
@@ -68,3 +75,22 @@ function createDirectoryContents(templatePath, newProjectPath) {
     }
   });
 }
+
+const pkg = name => ({
+  name: name,
+  version: "0.0.0",
+  private: true,
+  main: "index.js",
+  scripts: {
+    start: "nodemon"
+  },
+  dependencies: {
+    cors: "^2.8.4",
+    express: "^4.16.3",
+    helmet: "^3.13.0",
+    morgan: "^1.9.1"
+  },
+  devDependencies: {
+    nodemon: "^1.18.4"
+  }
+});
